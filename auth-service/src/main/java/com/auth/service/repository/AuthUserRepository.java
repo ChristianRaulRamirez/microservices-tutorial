45c13711b3c3a8720cf4dbb40f8d185d021bca29
ChristianRaulRamirez <cramirezchristian77@gmail.com>
2022-04-03 17:17:29 -0500

    auth-jwt

--- auth-service/src/main/java/com/auth/service/repository/AuthUserRepository.java
+++ auth-service/src/main/java/com/auth/service/repository/AuthUserRepository.java
@@ -3,9 +3,11 @@ package com.auth.service.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.auth.service.entity.AuthUser;
 
+@Repository
 public interface AuthUserRepository extends JpaRepository<AuthUser, Integer>{
 
 	Optional<AuthUser> findByUserName(String username);
